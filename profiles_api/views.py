fb851105aae86c8db40f30385e95051792585112
Dheeraj <kalwanidheeraj09@gmail.com>
2023-08-09 13:44:27 +0530

    Added views for hellow api and mapped it to url

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+
+class HelloApiView(APIView):
+
+    def get(self,request,format=None):
+        an_api = [
+            'This is GET',
+            'This is http method'
+        ]
+
+        return Response({'message':'hello','api_view':an_api})
