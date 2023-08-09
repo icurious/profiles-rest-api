c203acb68eadd866d975b48b9da97f161e0e95fd
Dheeraj <kalwanidheeraj09@gmail.com>
2023-08-09 16:18:00 +0530

    Added list,retrieve,update,partial_update,destroy

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,7 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 
 from profiles_api import views 
 
+router = DefaultRouter()
+router.register('hellow-viewset', views.HelloViewSets, base_name = 'hello-viewset')
+
 urlpatterns = [
-    path('hellow-view/', views.HelloApiView.as_view())
+    path('hellow-view/', views.HelloApiView.as_view()),
+    path('', include(router.urls))
+
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
-
+from rest_framework import viewsets
 # Create your views here.
 
 
@@ -38,3 +38,40 @@ class HelloApiView(APIView):
 
     def delete(self,request,pk=None):
         return Response({'message': 'DELETE'})
+
+
+class HelloViewSets(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializers
+
+    def list(self,request):
+        a_list = [
+            'This is viewset',
+            'This is list'
+        ]
+
+        return Response({'message': 'hello', 'list': a_list})
+    
+    def create(self,request):
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+        
+        name = serializer.validated_data.get('name')
+
+        message = 'Hello {}'.format(name)
+
+        return Response({'message': message})
+    
+    def retrieve(self,request,pk=None):
+        return Response({'http': 'GET'})
+
+    def update(self,request,pk=None):
+        return Response({'http': 'PUT'})
+
+    def partial_update(self,request,pk=None):
+        return Response({'http': 'PATCH'})
+
+    def delete(self,request,pk=None):
+        return Response({'http': 'DELETE'})
